68ba05998238eaa95e88e6b71f96bbff64047dae
supriadi <supriadi96@gmail.com>
2024-09-01 11:16:38 +0700

    add param to get_total_pages

--- scrape_total_pages.py
+++ scrape_total_pages.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import os
 
 
-def get_total_pages(url):
+def get_total_pages(url, query=None, location=None):
 
     headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.3',
@@ -14,6 +14,13 @@ def get_total_pages(url):
     'Connection': 'keep-alive',
     }
 
+    if query:
+        url = f'{url}/id/{query}'
+
+        if location:
+            url = '{}/{}'.format(url, location)
+
+    #print(url)
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
         raise SystemError
@@ -23,14 +30,13 @@ def get_total_pages(url):
     except FileExistsError:
         pass
 
-    with open('temp/res.html', 'w+') as outfile:
+    with open('temp/res.html', 'w') as outfile:
         outfile.write(response.text)
 
     #scraping step
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # find tag ul and class _1decxdv0 _1decxdv3 _110qf3s5a _110qf3sfq
-    pagination = soup.find('ul', '_1decxdv0 _1decxdv3 _110qf3s5a _110qf3sfq')
+    pagination = soup.find('nav', {'aria-label':'Penomoran halaman hasil'})
 
     # find all tag li in the element
     pages = pagination.find_all('li')
